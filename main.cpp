987ae94c8353be4a3eea778332ab6c58ad6dd075
PrzemyslawSochon <przemyslaw.sochon+github@protonmail.ch>
2022-12-13 07:08:25 +0100

    tab formatting

--- main.cpp
+++ main.cpp
@@ -6,50 +6,50 @@
 
 int main()
 {
-  bool wanna_play{true};
-  while(wanna_play)
-  {
-
-  Deck deck{};
-
-  try
-  {
-  Result result{playBlackjack(deck)};
-
-  switch(result)
+    bool wanna_play{ true };
+    while (wanna_play)
     {
-      case Result::lose:
-      {
-        std::cout << "You lost!\n";
-        wanna_play=askForReplay();
-        break;
-      }
-      case Result::win:
-      {
-        std::cout << "You won!\n";
-        wanna_play=askForReplay();
-        break;
-      }
-      case Result::tie:
-      {
-        std::cout << "The game ended in a draw!\n";
-        wanna_play=askForReplay();
-        break;
-      }
-      default:
-          assert(false && "should never happen");
-          return 1;
+
+        Deck deck{};
+
+        try
+        {
+            Result result{ playBlackjack(deck) };
+
+            switch (result)
+            {
+            case Result::lose:
+            {
+                std::cout << "You lost!\n";
+                wanna_play = askForReplay();
+                break;
+            }
+            case Result::win:
+            {
+                std::cout << "You won!\n";
+                wanna_play = askForReplay();
+                break;
+            }
+            case Result::tie:
+            {
+                std::cout << "The game ended in a draw!\n";
+                wanna_play = askForReplay();
+                break;
+            }
+            default:
+                assert(false && "should never happen");
+                return 1;
+            }
+        }
+        catch (const std::exception& e)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cerr << e.what() << "Error, try again.\n";
+        }
     }
-  }
-  catch(const std::exception& e)
-  {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cerr << e.what() << "Error, try again.\n";
-  }
-  }
 
-  std::cout << "Thank you for playing! The casino is closing down...\n";
+    std::cout << "Thank you for playing! The casino is closing down...\n";
 
-  return 0;
+    return 0;
 }
